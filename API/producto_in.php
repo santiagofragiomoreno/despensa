b6ff9172ba873053e1b9096cc9e7eca068db3d07
santiagofragiomoreno <santiagofragio@gmail.com>
2019-04-21 19:38:59 +0200

    #15 arreglo en producto_in

--- API/producto_in.php
+++ API/producto_in.php
@@ -11,31 +11,102 @@ $update = 0;
 //comprobamos si nos llegan los parametros de la RASPBERRY
 if(isset($_POST['codigo_producto']) && isset($_POST['usuario']) && isset($_POST['peso'])){
     //actualizamos el peso del producto
-    $consulta = "UPDATE productos SET peso = ".$_POST['peso']." WHERE id_usuario =".$_POST['usuario']." AND codigo_producto =".$_POST['codigo_producto'];
-    if($conexion->query($consulta) === true ){
-        echo $update = 1;
-        //recuperamos el id del producto del usuario
-        $consulta = "SELECT id FROM productos WHERE codigo_producto = ".$_POST['codigo_producto']." AND id_usuario = ".$_POST['usuario'];
-        $id_producto = $conexion->query($consulta);
-        $contador = 0;
-        $id = array();
-        $ok = 0;
-        if($id_producto->num_rows>0){
-            while ($fila = $id_producto->fetch_assoc()){
-                //$estudiantes[$contador]["id"]       = $fila["id"];
-                $id[$contador]["id"]   = $fila["id"];
-                $contador++;
+    //primero miramos si ya hay peso de ese producto metido
+    //recuperamos el peso que haya de ese producto
+    $consulta = "SELECT peso FROM productos WHERE codigo_producto = ".$_POST['codigo_producto']." AND id_usuario = ".$_POST['usuario'];
+    $resultado_consulta = $conexion->query($consulta);//nos devuelve un objeto
+    $contador = 0;
+    $peso = array();
+    if($resultado_consulta->num_rows>0){
+        while ($fila = $resultado_consulta->fetch_assoc()){
+            //$estudiantes[$contador]["id"]       = $fila["id"];
+            $peso[$contador]["peso"]   = $fila["peso"];
+            $contador++;
+        }
+    }
+    // si hubiera un producto igual introducido ya en la despensa ya habria un peso > 0
+    if($peso[0]["peso"] > 0){
+        $peso[0]["peso"] = $peso[0]["peso"] + $_POST['peso'];
+        $consulta = "UPDATE productos SET peso = ".$peso[0]["peso"] ." WHERE id_usuario = ".$_POST['usuario']." AND codigo_producto = ".$_POST['codigo_producto'];
+        $resultado_consulta = $conexion->query($consulta);//nos devuelve un objeto
+        if($resultado_consulta){
+            //recuperamos el id del producto del usuario para guardar el consumo
+            $consulta = "SELECT id FROM productos WHERE codigo_producto = ".$_POST['codigo_producto']." AND id_usuario = ".$_POST['usuario'];
+            $id_producto = $conexion->query($consulta);
+            $contador = 0;
+            $id = array();
+            if($id_producto->num_rows>0){
+                while ($fila = $id_producto->fetch_assoc()){
+                    //$estudiantes[$contador]["id"]       = $fila["id"];
+                    $id[$contador]["id"]   = $fila["id"];
+                    $contador++;
+                }
+            }
+            $consulta = "INSERT INTO consumo_productos (id_producto,id_usuario,consumo_producto) VALUES ('".$id[0]["id"]."','".$_POST['usuario']."','".$_POST['peso']."')";
+            if($conexion->query($consulta) === true){
+                $update = 1;
             }
         }
-        $fecha_consumo = date("d-m-Y");
-        $consulta = "INSERT INTO consumo_productos (id_producto,id_usuario,consumo_producto) VALUES ('".$id[0]["id"]."','".$_POST['usuario']."','".$_POST['peso']."')";
-        if($conexion->query($consulta) === true){
-            $ok = 1;
+        else{
+            $update = 0;
         }
     }
-    else{
-        echo $update = 0;
+    // si el producto lo tenemos a 0 gramos
+    elseif($peso[0]["peso"] == 0){
+        $peso[0]["peso"] = $_POST['peso'];
+        $consulta = "UPDATE productos SET peso = ".$peso[0]["peso"] ." WHERE id_usuario = ".$_POST['usuario']." AND codigo_producto = ".$_POST['codigo_producto'];
+        $resultado_consulta = $conexion->query($consulta);//nos devuelve un objeto
+        if($resultado_consulta){
+            //recuperamos el id del producto del usuario para guardar el consumo
+            $consulta = "SELECT id FROM productos WHERE codigo_producto = ".$_POST['codigo_producto']." AND id_usuario = ".$_POST['usuario'];
+            $id_producto = $conexion->query($consulta);
+            $contador = 0;
+            $id = array();
+            if($id_producto->num_rows>0){
+                while ($fila = $id_producto->fetch_assoc()){
+                    //$estudiantes[$contador]["id"]       = $fila["id"];
+                    $id[$contador]["id"]   = $fila["id"];
+                    $contador++;
+                }
+            }
+            $consulta = "INSERT INTO consumo_productos (id_producto,id_usuario,consumo_producto) VALUES ('".$id[0]["id"]."','".$_POST['usuario']."','".$_POST['peso']."')";
+            if($conexion->query($consulta) === true){
+                $update = 1;
+            }
+        }
+        else{
+            $update = 0;
+        }
     }
+    //si por lo que fuese su hubiera quedado un valor negativo
+    else{
+        $peso[0]["peso"] = 0;
+        $peso[0]["peso"] = $peso[0]["peso"] + $_POST['peso'];
+        $consulta = "UPDATE productos SET peso = ".$peso[0]["peso"] ." WHERE id_usuario = ".$_POST['usuario']." AND codigo_producto = ".$_POST['codigo_producto'];
+        $resultado_consulta = $conexion->query($consulta);//nos devuelve un objeto
+        if($resultado_consulta){
+            //recuperamos el id del producto del usuario para guardar el consumo
+            $consulta = "SELECT id FROM productos WHERE codigo_producto = ".$_POST['codigo_producto']." AND id_usuario = ".$_POST['usuario'];
+            $id_producto = $conexion->query($consulta);
+            $contador = 0;
+            $id = array();
+            if($id_producto->num_rows>0){
+                while ($fila = $id_producto->fetch_assoc()){
+                    //$estudiantes[$contador]["id"]       = $fila["id"];
+                    $id[$contador]["id"]   = $fila["id"];
+                    $contador++;
+                }
+            }
+            $consulta = "INSERT INTO consumo_productos (id_producto,id_usuario,consumo_producto) VALUES ('".$id[0]["id"]."','".$_POST['usuario']."','".$_POST['peso']."')";
+            if($conexion->query($consulta) === true){
+                $update = 1;
+            }
+        }
+        else{
+            $update = 0;
+        }
+    }
+    echo $update;
 }
 else{
     echo $update = 0;
