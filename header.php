b914d2547edb4703911efad15578a902a9012414
santiagofragiomoreno <santiagofragio@gmail.com>
2019-04-25 12:05:22 +0200

    prueba3

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-<?php if($pagina > 0){?>
+
 		<p style="border: 1px solid #000">
 			<a href="<?php echo DIRECTORIO_BASE."home";?>">HOME</a>
 		</p>
-<?php }?>
+
