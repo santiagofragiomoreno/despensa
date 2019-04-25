2e36b8486b8cf4cf19757910761937b4aa752f07
santiagofragiomoreno <santiagofragio@gmail.com>
2019-04-25 13:28:33 +0200

    jquery y js

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 
 <?php
 ?>
-<script ></script>
+<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+<script src="js/bootstrap.js"></script>
 <p style="border: 1px solid #000">footer</p>
 </body>
 
