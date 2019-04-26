28dcf685a9a400d4a1ad0b9a4dad6c3f1d718c5c
santiagofragiomoreno <santiagofragio@gmail.com>
2019-04-26 14:23:46 +0200

    cambio lolgo facebook

--- footer.php
+++ footer.php
@@ -5,10 +5,22 @@
 <script src="js/bootstrap.js"></script>
 <div class="container footer">
 <a href="#">
-<img class="color-favicon" src="https://img.icons8.com/windows/32/000000/facebook.png">
+<img><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 192 192" style=" fill:#000000;">
+		<g transform="">
+		<g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+			<path d="M0,192v-192h192v192z" fill="none"></path>
+			<path d="M0,192v-192h192v192zM188.16,188.16v-184.32h-184.32v184.32z" fill="#ffffff"></path>
+			<g fill="#ffffff"><g id="surface1">
+			<path d="M52.8,43.2c-5.25,0 -9.6,4.35 -9.6,9.6v86.4c0,5.25 4.35,9.6 9.6,9.6h86.4c5.25,0 9.6,-4.35 9.6,-9.6v-86.4c0,-5.25 -4.35,-9.6 -9.6,-9.6zM52.8,52.8h86.4v86.4h-24.9v-32.4h12.45l1.8,-14.4h-14.25v-9.3c0,-4.2 1.03125,-7.05 7.05,-7.05h7.8v-13.05c-1.33125,-0.16875 -5.94375,-0.45 -11.25,-0.45c-11.0625,0 -18.6,6.65625 -18.6,19.05v10.8h-12.6v14.4h12.6v32.4h-46.5z"></path>
+			</g></g><path d="" fill="none"></path>
+			<path d="" fill="none"></path>
+			<path d="M96,192c-53.01934,0 -96,-42.98066 -96,-96v0c0,-53.01934 42.98066,-96 96,-96v0c53.01934,0 96,42.98066 96,96v0c0,53.01934 -42.98066,96 -96,96z" fill="none"></path>
+			<path d="M96,188.16c-50.89856,0 -92.16,-41.26144 -92.16,-92.16v0c0,-50.89856 41.26144,-92.16 92.16,-92.16v0c50.89856,0 92.16,41.26144 92.16,92.16v0c0,50.89856 -41.26144,92.16 -92.16,92.16z" fill="none"></path>
+			<path d="M0,192v-192h192v192z" fill="none"></path>
+			<path d="M3.84,188.16v-184.32h184.32v184.32z" fill="none"></path></g></g></svg>
 </a>
 <a>
-<img class="color-favicon" src="https://img.icons8.com/windows/32/000000/instagram-new.png">
+<img src="https://img.icons8.com/windows/32/000000/instagram-new.png">
 </a>
 </div>
 </body>
