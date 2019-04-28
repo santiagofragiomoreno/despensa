52f8963a869dfc5e222fd1f2aea75840adfc3d51
santiagofragiomoreno <santiagofragio@gmail.com>
2019-04-28 11:54:11 +0200

    header2

--- header.php
+++ header.php
@@ -14,8 +14,8 @@
 	<img class="logo1" alt="miwebdepruebas" src="img/logo_750.png">
 	</div>
 	<div class="container header2">
-	<a href="#">Quienes Somos</a>
-	<a href="#">Iniciar Sesion</a>
-	<a href="#">Contacto</a>
+	<a class="a_header2" href="#">Quienes Somos</a>
+	<a class="a_header2" href="#">Iniciar Sesion</a>
+	<a class="a_header2" href="#">Contacto</a>
 	</div>
 
