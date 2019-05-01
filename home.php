94fc80eac5a7fe187ccd3a41b2cef6e0023eb33f
santiagofragiomoreno <santiagofragio@gmail.com>
2019-05-01 18:54:27 +0200

    #61 home2

--- home.php
+++ home.php
@@ -1,10 +1,5 @@
 <?php
 ?>
-<div class="container header">
+<div class="container home">
 	<img class="logo1" alt="miwebdepruebas" src="img/logo_750.png">
-	</div>
-	<div class="container header2">
-	<a class="a_header2" href="#">Quienes Somos</a>
-	<a class="a_header2" href="#">Iniciar Sesion</a>
-	<a class="a_header2" href="#">Contacto</a>
-	</div>
+</div>
