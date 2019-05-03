16fd04bffc14717ba689701fd48392e8f3e43032
santiagofragiomoreno <santiagofragio@gmail.com>
2019-05-03 13:59:25 +0200

    header home 2

--- header.php
+++ header.php
@@ -13,9 +13,16 @@
 	<div class="container header">
 	<img class="logo1" alt="miwebdepruebas" src="img/logo_750.png">
 	</div>
-	<div class="container header2">
-	<a class="a_header2" href="#">Quienes Somos</a>
-	<a class="a_header2" href="#">Iniciar Sesion</a>
-	<a class="a_header2" href="#">Contacto</a>
+	<div class="container">
+		<div class="container header1">
+			<a class="a_header2" href="<?php echo DIRECTORIO_BASE?>home2">Inicio</a>
+			<a class="a_header2" href="#">Quienes Somos</a>
+			<a class="a_header2" href="#">Que Hacemos</a>
+			<a class="a_header2" href="#">Que Hacemos</a>
+		</div>
+		<div class="container header2">
+		<a class="a_header2" href="#">Iniciar Sesion</a>
+		<a class="a_header2" href="#">Contacto</a>
+		</div>
 	</div>
 
