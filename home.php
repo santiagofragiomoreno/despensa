57f2bc693a9f5f68c50bd4817644e7d408159ecc
santiagofragiomoreno <santiagofragio@gmail.com>
2019-05-03 12:55:32 +0200

    margin iconos

--- home.php
+++ home.php
@@ -15,15 +15,15 @@
 	</div>
 	<div class="container imageneshome">
 	    <a class="imghome" href="#">
-		<img style="width:150px;" alt="espana" src="img/espana.png">
+		<img style="width:150px; margin:10px;" alt="espana" src="img/espana.png">
 		</a>
 		<a class="imghome" href="#">
-		<img style="width:150px;" alt="portugal" src="img/portugal.png">
+		<img style="width:150px; margin:10px;" alt="portugal" src="img/portugal.png">
 		</a>
 		<a class="imghome" href="#">
-		<img style="width:150px;" alt="francia" src="img/francia.png">
+		<img style="width:150px; margin:10px;" alt="francia" src="img/francia.png">
 		</a>
 		<a class="imghome" href="#">
-		<img style="width:150px;" alt="mexico" src="img/mexico.png">
+		<img style="width:150px; margin:10px;" alt="mexico" src="img/mexico.png">
 		</a>
 	</div>
