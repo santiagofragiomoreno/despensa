110d0291e89b68b62becee1712c10f4e8596a014
santiagofragiomoreno <santiagofragio@gmail.com>
2019-05-03 14:19:02 +0200

    enlaces 3 home2

--- header.php
+++ header.php
@@ -14,15 +14,11 @@
 	<img class="logo1" alt="miwebdepruebas" src="img/logo_750.png">
 	</div>
 	<div class="container">
-		<div class="container header1">
-			<a class="a_header2" href="<?php echo DIRECTORIO_BASE?>home2">Inicio</a>
-			<a class="a_header2" href="#">Quienes Somos</a>
-			<a class="a_header2" href="#">Que Hacemos</a>
-			<a class="a_header2" href="#">Que Hacemos</a>
-		</div>
-		<div class="container header2">
-		<a class="a_header2" href="#">Iniciar Sesion</a>
-		<a class="a_header2" href="#">Contacto</a>
-		</div>
+		<a class=" header1 a_header2" href="<?php echo DIRECTORIO_BASE?>home2">Inicio</a>
+		<a class=" header1 a_header2" href="#">Quienes Somos</a>
+		<a class=" header1 a_header2" href="#">Que Hacemos</a>
+		<a class=" header1 a_header2" href="#">Contacto</a>
+		<a class=" header2 a_header2" href="#">Iniciar Sesion</a>
+		<a class=" header2 a_header2" href="#">Registro</a>
 	</div>
 
