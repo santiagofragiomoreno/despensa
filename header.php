28d0e424a31fae4941e7836d1d8f9c7b59d7bc9a
santiagofragiomoreno <santiagofragio@gmail.com>
2019-05-04 21:10:20 +0200

    previo home2

--- header.php
+++ header.php
@@ -8,17 +8,22 @@
 <link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/estilos.css">
 <title>Insert title here</title>
+
 </head>
 <body>
 	<div class="container header">
 	<img class="logo1" alt="miwebdepruebas" src="img/logo_750.png">
 	</div>
-	<div class="container">
-		<a class=" header1 a_header2" href="<?php echo DIRECTORIO_BASE?>home2">Inicio</a>
-		<a class=" header1 a_header2" href="#">Quienes Somos</a>
-		<a class=" header1 a_header2" href="#">Que Hacemos</a>
-		<a class=" header1 a_header2" href="#">Contacto</a>
-		<a class=" header2 a_header2" href="#">Iniciar Sesion</a>
-		<a class=" header2 a_header2" href="#">Registro</a>
+	<div class="container div1">
+		<div class="div1-1">
+			<a class=" header1 a_header2" href="<?php echo DIRECTORIO_BASE?>home2">Inicio</a>
+			<a class=" header1 a_header2" href="#">Quienes Somos</a>
+			<a class=" header1 a_header2" href="#">Que Hacemos</a>
+			<a class=" header1 a_header2" href="#">Contacto</a>
+		</div>
+		<div class="div1-2">
+			<a class=" header2 a_header2" href="#">Iniciar Sesion</a>
+			<a class=" header2 a_header2" href="#">Registro</a>
+		</div>
 	</div>
 
