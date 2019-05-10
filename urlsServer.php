bbaf2879e3223180e26944773107a12b875c3b1a
santiagofragiomoreno <santiagofragio@gmail.com>
2019-05-10 18:08:33 +0200

    prueba youitt.com

--- urlsServer.php
+++ urlsServer.php
@@ -1,4 +1,4 @@
 <?php
-const DIRECTORIO_BASE = "http://www.miwebdepruebas.es/";
-const RUTA_API = "http://www.miwebdepruebas.es/API/";
+const DIRECTORIO_BASE = "http://www.youitt.com/";
+const RUTA_API = "http://www.youitt.com/API/";
 ?>
